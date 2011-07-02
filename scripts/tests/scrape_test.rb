445b620d52f165b39791adb21877009711c1bec5
guy <frank@frank-VirtualBox.(none)>
2011-07-01 23:15:22 -0500

    scrape.rb:read_TG tested

--- scripts/tests/scrape_test.rb
+++ scripts/tests/scrape_test.rb
@@ -2,11 +2,25 @@
 # Author: Frank Lockom
 # tests for scrape.rb functions
 
+require 'test/unit'
 require_relative '../scrape.rb'
 
 class ScrapeTest < Test::Unit::TestCase
   def test_read_TG
     #first test without comments
-    read_TG('task-graphs/test_read_TG1')
+    tg = read_TG('task-graphs/test_read_TG1')
+    assert_equal(4,tg[0].size)
+    assert_equal(3,tg[1].size)
+    assert_equal([0,1,5],tg[1][0])
+    assert_equal([0,8,2],tg[1][1])
+    assert_equal([0,10,3],tg[1][2])
+
+    #now test with comments
+    tg = read_TG('task-graphs/test_tfo1')
+    assert_equal(4,tg[0].size)
+    assert_equal(3,tg[1].size)
+    assert_equal([0,1,5],tg[1][0])
+    assert_equal([0,8,2],tg[1][1])
+    assert_equal([0,10,3],tg[1][2])
   end
 end
