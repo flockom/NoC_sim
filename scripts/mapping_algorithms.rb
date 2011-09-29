042576ab44a86a1d536f79d45322884ec27e2a94
Frank Lockom <flockom@iit.edu>
2011-09-29 00:40:58 -0500

    ability to specify redundant cores in mapping

--- scripts/mapping_algorithms.rb
+++ scripts/mapping_algorithms.rb
@@ -4,21 +4,38 @@
 # methods to map a task graph to nxm cores.
 # where n=#cols and m=#rows
 # returns a new task graph where the node id is the core its mapped to
- 
-
+# tasks will not be mapped to redundant cores 
 
 
 # how to handle location of redundant cores?
-def random_mapping(tg,n,m)
-  #so we need a random sequence of unique numbers between 0 and nxm-1
+#  -specifiy the redundant cores in an array redundant  
+#  -the #tasks must be less than n*m-redundant
+#  -nthe index in the array refers to the task # which goes from 0 to #tasks
+#  -so swap all the redundant mappings to the end of the mapping array
+#  -then do the mapping... none of the tasks will be mapped to redundant cores
+
+def random_mapping(tg,n,m,redundant)
+  #so we need a random sequence of unique numbers between 0 and nxm-1-redundant
   #... and thats our mapping!
-  s = n*m
-  mapping = Array.new(s) {|i| i}
+  s = n*m-redundant.size
+  mapping = Array.new(n*m) {|i| i}
+
+  #swap the redundant cores to the end
+  redundant.each_with_index do |r,i| 
+    mapping[r],mapping[s+i] = [mapping[s+i],mapping[r]]
+  end
+
+  puts mapping
+  puts "hokay?"
+  
+  #swap around the remining cores to get the mapping
   (s).times do 
     a,b = [rand(s),rand(s)]
     mapping[a],mapping[b] = [mapping[b],mapping[a]]
   end
-  
+
+  puts mapping
+
   #now build the new tg from the old one
   result = [Array.new(tg[0].size),Array.new(tg[1].size)]
 
