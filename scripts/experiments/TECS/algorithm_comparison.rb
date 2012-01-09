61e353b3d36fb4ba0afe662c8b184d7e00abab03
Frank Lockom <flockom@iit.edu>
2012-01-09 05:14:08 -0600

    TECS algorithm comparison experiment finished

--- scripts/experiments/TECS/algorithm_comparison.rb
+++ scripts/experiments/TECS/algorithm_comparison.rb
@@ -10,10 +10,10 @@ start_time = Time.now
 mesh_sizes = [[4,3],[5,4],[6,5],[7,6]]
 
 # generate num_graphs task graphs per mesh
-num_graphs = 1
+num_graphs = 5
 
 # generate num_faulty faulty sets per graph
-num_faulty = 1
+num_faulty = 5
 
 # algorithms to test
 # [method(:random),method(:greedy),method(:brute_force_optimal)]
@@ -70,16 +70,80 @@ solns = Array.new(algs.size) do |alg|
 end
 
 
-
 # for each mesh size, average the solutions for each algorithm
 averages = Array.new(algs.size) do |alg|
-  {algs[alg].name =>
+
   Array.new(mesh_sizes.size) do |mesh| 
     solns[alg][mesh].reduce(:+)/solns[alg][mesh].size.to_f
   end
-  }
+
 end
+
 
 puts averages.to_s
 
 puts "Total run-time: #{Time.now - start_time} seconds"
+
+
+#write data out for gnuplot
+File.open("temp.dat","w") do |hist|
+  
+  mesh_sizes.size.times do |mesh|
+    algs.size.times do |alg|
+      hist.write("#{averages[alg][mesh]} ")
+    end
+    hist.write("\n")
+  end
+end
+
+
+
+#write out the gnuplot commands
+
+#get the xtics i.e 3x3, 4x4, 5x5
+xtics = ""
+mesh_sizes.size.times do |i|
+  xtics += "\"#{mesh_sizes[i][1]}x#{mesh_sizes[i][1]}\" #{i+1} -1#{(i == mesh_sizes.size-1 )?"":", "}"
+end
+
+# plot command looks something like this:
+# plot 'temp.dat' using 1 t "optimal",'' using 2 t "greedy",'' using 3 t "hungarian",'' using 4 t "random"
+plot = "'temp.dat' using 1 t \"#{algs[0].name}\""
+(2..(algs.size)).each do |alg|
+  plot += ", '' using #{alg} t \"#{algs[alg-1].name}\""
+end
+
+gnuplot = <<END
+set terminal png
+set output "algorithm_comp.png"
+
+set style data histogram
+set style histogram cluster gap 1
+set style fill solid 1.00 border -1
+
+set format y ""
+set format x ""
+
+set key horizontal
+
+
+set xtics norangelimit
+set xtics (#{xtics})
+
+set title  "Algorithm Comparison of #{mesh_sizes.size} virtual topology sizes \\n each with #{num_graphs} task graphs and #{num_faulty} faulty sets per graph"
+
+set xlabel "virtual topology size (rows x columns)"
+set ylabel "Average Starting time distance from reference topology"
+
+
+plot #{plot}
+
+
+END
+File.open("temp.gnu", 'w') do |f|
+  f.write gnuplot
+end
+
+#run gnuplot
+`gnuplot temp.gnu`
+
